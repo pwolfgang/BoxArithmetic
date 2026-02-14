ea0d7232778ca4b375e0eb2ceb0222b25fcf767a
Paul Wolfgang <paul@pwolfgang.com>
2026-02-14 17:54:37 -0500

    Added Singleton

---
+++ src/main/java/com/pwolfgang/boxarithmetic/Singleton.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2026 Paul
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.pwolfgang.boxarithmetic;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * A Singleton is a list containing a single box. The paper restricts singletons
+ * to boxes of natural numbers, but the lecture removes this restriction.
+ * @author Paul
+ */
+public class Singleton extends NonEmptyBox {
+    
+    public Singleton(Box box) {
+        super(List.of(box));
+    }
+    
+    @Override
+    public String toString() {
+        var stj = new StringJoiner(" ", "\u2308", "\u2309");
+        for (Box b : content.getFirst()) {
+            stj.add(b.toString());
+        }
+        return stj.toString();  
+    }
+    
+    @Override
+    public String toIntegerString() {
+        return "\u2308" + content.getFirst().toIntegerString() + "\u2309";
+    }
+    
+   
+    public String toRawString() {
+        return super.toString();
+    }
+    
+}
---
+++ src/test/java/com/pwolfgang/boxarithmetic/SingletonTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2026 Paul
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.pwolfgang.boxarithmetic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Paul
+ */
+public class SingletonTest {
+    void printIt(String s, Box box) {
+        System.out.printf("%s: %s%n", s, box.toString());
+        System.out.printf("%s: %s%n", s, box.toIntegerString());
+        System.out.printf("%s: %s%n", s, box.asPolyNumber());
+    }
+
+    public SingletonTest() {
+    }
+
+    @Test
+    public void testToString() {
+        var test = Box.parse("[0]");
+        printIt("test", test);
+        var e0 = new Singleton(Box.of(0));
+        System.out.printf("e0: %s%n", e0.toRawString());
+        printIt("e0", e0);
+        var e1 = new Singleton(Box.of(1));
+        System.out.printf("e1: %s%n", e1.toRawString());
+        printIt("e1", e1);
+        var e2 = new Singleton(Box.of(2));
+        System.out.printf("e2: %s%n", e2.toRawString());
+        printIt("e2", e2);
+        var e3 = new Singleton(Box.of(3));
+        System.out.printf("e3: %s%n", e3.toRawString());
+        printIt("e3", e3);
+        var e4 = new Singleton(Box.of(4));
+        System.out.printf("e4: %s%n", e4.toRawString());
+        printIt("e4", e4);
+    }
+    
+}
